bc4717c71bac67d03a8e9e5e1b897a0f4f8fc863
Romain <romain.pillot@epitech.eu>
2017-05-22 14:49:44 +0000

    Adding base files

---
+++ inc/config.h
@@ -0,0 +1,16 @@
+/*
+** dante.h for  in /home/romain.pillot/projects/dante/astar/inc
+** 
+** Made by romain pillot
+** Login   <romain.pillot@epitech.net>
+** 
+** Started on  Thu Apr 27 13:47:35 2017 romain pillot
+** Last update Mon May 22 14:09:34 2017 romain pillot
+*/
+
+#ifndef CONFIG_H_
+# define CONFIG_H_
+
+
+
+#endif /* !CONFIG_H_ */
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for  in /home/romain.pillot/projects/ghoulc/src
+** 
+** Made by romain pillot
+** Login   <romain.pillot@epitech.net>
+** 
+** Started on  Mon May 22 14:09:17 2017 romain pillot
+** Last update Mon May 22 14:20:56 2017 romain pillot
+*/
+
+#include "config.h"
+
+int	main(int ac, char **args)
+{
+  
+  return (0);
+}
