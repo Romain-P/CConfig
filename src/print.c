d6cdfef818057d4311f6923846258d1b3ef1587e
Romain <romain.pillot@epitech.eu>
2017-05-23 14:46:05 +0000

    Fixing print channels

--- src/print.c
+++ src/print.c
@@ -5,7 +5,7 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Tue May 23 13:51:46 2017 romain pillot
-** Last update Tue May 23 14:15:07 2017 romain pillot
+** Last update Tue May 23 14:45:53 2017 romain pillot
 */
 
 #include <stdio.h>
@@ -18,7 +18,7 @@ static bool	private_print(const int channel, const char *str)
 
   len = 0;
   while(str && str[len] && ++len);
-  return (len ? write(STDOUT_FILENO, str, len) : false);
+  return (len ? write(channel, str, len) : false);
 }
 
 bool	print(const char *str)
